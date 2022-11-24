6b6163ddbfc3744198c3c1e3cab364b6107d4f1c
erlang alfatah <erlangalfatah12@gmail.com>
2022-11-24 15:16:31 +0700

    Update SmartPlugProgram.ino

--- SmartPlugProgram.ino
+++ SmartPlugProgram.ino
@@ -3,8 +3,8 @@
 #include <WiFiManager.h>
 
 //Firebase autentikasi 
- #define FIREBASE_HOST "https://areauji-default-rtdb.firebaseio.com" //Sesuaikan dengan link firebase database kalian, tanpa menggunakan "http:" , "\" and "/"
- #define FIREBASE_AUTH "Bn7LhCTiP8VphRn53K2WmcqlHvV6WnexeRj7vKLa3ns" //Sesuaikan dengan firebase database secret code kalian
+ #define FIREBASE_HOST "https://sosmart-technopark-default-rtdb.firebaseio.com/" //Sesuaikan dengan link firebase database kalian, tanpa menggunakan "http:" , "\" and "/"
+ #define FIREBASE_AUTH "afL12P1vXA0goN4ia42rsC4DUYLnNMqI19XRIye6" //Sesuaikan dengan firebase database secret code kalian
 
 //Nama hotspot jika tak tersambung
  #define NamaHotspot "ControlLampFirebase" // Nama hotspot di esp8266
@@ -12,58 +12,55 @@
 //Pzem sensor
  PZEM004Tv30 pzem(12,13);// D6 dan D7
  float Power,Energy,Voltase,Current;
-
+ 
 // Firebase
  FirebaseData firebaseData;
- #define lamp_saklar1 5 //D1
- #define lamp_saklar2 4 //D2
- #define saklar1 0 //D3
- #define saklar1 2 //D4
+ #define Lamp_saklar1 5 //D1
+ #define Lamp_saklar2 4 //D2
+ #define Saklar1 0 //D3
+ #define Saklar2 2 //D4
  String val1;
  String val2;
  String val3;
  String val4;
  String zero = "0" , one = "1";
 
-
-
-
 void setup(){
+    
 Serial.begin(115200);
    
    //WifiManager:
     WiFiManager wm;
-
     wm.resetSettings();
-
     bool res;
-
     res = wm.autoConnect("NamaHotspot");
-
     if(!res) {
-
-     Serial.println("Failed to connect");
-
-     // ESP.restart();
-
+    Serial.println("Failed to connect");
     } 
-
     else {
-
-        //if you get here you have connected to the WiFi    
-
-        Serial.println("connected...yeey :)");
-
+     Serial.println("connected...yeey :)");
     }
     WiFi.mode(WIFI_STA);
-
-
+    
+   //Firebase
+    Serial.print('\n');
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); // connect to firebase
+    Firebase.reconnectWiFi(true);
+    Firebase.setString(firebaseData, "Product/1BFOAB5PL482/Saklar2","0");
+    Firebase.setString(firebaseData, "Product/1BFOAB5PL482/Saklar2","1");
+    
+   //PinMode
+    pinMode(Saklar1,OUTPUT);
+    pinMode(Saklar2,OUTPUT);
+    pinMode(Lamp_saklar1,OUTPUT);
+    pinMode(Lamp_saklar2,OUTPUT);
+   
 }
 
 void loop(){
     
-   //Read power
-    Power = pzem.power();
+ //Read power
+ Power = pzem.power();
     
     if(isnan(Power)){
         Serial.print("Mati");
@@ -99,14 +96,51 @@ void loop(){
     }
     
     //Read Current
-    Current = pzem.current();
+     Current = pzem.current();
     
-    if(isnan(Current)){
+     if(isnan(Current)){
         Serial.print("Mati");
-    }
-    else{
+     }
+     else{
         Serial.print("Current : ");
         Serial.print(Current);
         Serial.println("A");
-    }
+     }
+   
+    //Firebase Saklar1
+     Firebase.getString(firebaseData, "Product/1BFOAB5PL482/Saklar1");
+     val1 = firebaseData.stringData(); Serial.print('\n');
+
+     Serial.print("Value 1 ");
+     Serial.println(val1);
+
+     if(val1==one){ 
+      digitalWrite(Saklar1,HIGH);
+      digitalWrite(Lamp_saklar1,HIGH);
+      Serial.println("Saklar1 ON");
+      } 
+     else if(val1==zero){ 
+      digitalWrite(Saklar1,LOW);
+      digitalWrite(Lamp_saklar1,LOW);
+      Serial.println("Saklar 1 OFF");
+      }
+    
+    //Firebase Saklar2
+     Firebase.getString(firebaseData, "Product/1BFOAB5PL482/Saklar2");
+     val2 = firebaseData.stringData(); Serial.print('\n');
+
+     Serial.print("Value 2 ");
+     Serial.println(val2);
+
+     if(val2==one){ 
+      digitalWrite(Saklar2,HIGH);
+      digitalWrite(Lamp_saklar2,HIGH);
+      Serial.println("Saklar2 ON");
+      } 
+     else if(val2==zero){ 
+      digitalWrite(Saklar2,LOW);
+      digitalWrite(Lamp_saklar2,LOW);
+      Serial.println("Saklar 2 OFF");
+    }    
+  
 }
